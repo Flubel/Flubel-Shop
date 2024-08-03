039def2772dcd25b44798284caf19b8734460d64
MrFiend179 <yibolaxinw@gmail.com>
2024-08-03 17:17:04 +0500

    Initial Commit

---
+++ src/api.js
@@ -0,0 +1,32 @@
+const express = require("express")
+const serverless = require("serverless-http")
+
+
+const app = express()
+const router = express.Router()
+
+router.get('/',(req,res)=>{
+    res.json({
+        "hello": "hi"
+    })
+})
+
+
+router.get('/welcome',(req,res)=>{
+    res.json({
+        "hello": "welcomed"
+    })
+})
+
+
+
+router.get('/ola',(req,res)=>{
+    res.json({
+        "hello": "ola"
+    })
+})
+
+
+app.use('/.netlify/functions/api', router)
+
+module.exports.handler = serverless(app)
